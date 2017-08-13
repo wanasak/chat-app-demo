c1927f99ced151e99bdbee76ea21dd5f810e836a
Wanasak <u510610433@gmail.com>
2017-08-14 01:21:56 +0700

    using onChanges to scroll to the last message

--- src/app/message-list/message-list.component.ts
+++ src/app/message-list/message-list.component.ts
@@ -1,18 +1,44 @@
 import { Message } from './../../../shared/model/message';
-import { Component, OnInit, Input } from '@angular/core';
+import {
+  Component,
+  Input,
+  ElementRef,
+  ViewChild,
+  OnChanges,
+  SimpleChanges
+} from '@angular/core';
+
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.css']
 })
-export class MessageListComponent implements OnInit {
+export class MessageListComponent implements OnChanges {
 
   @Input() messages: Message[];
 
-  constructor() { }
+  @ViewChild('list') list: ElementRef;
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['messages']) {
+      const previousMessages = changes['messages'].previousValue;
+      const newMessages = changes['messages'].currentValue;
+
+      if (newMessages.length > previousMessages.length) {
+        setTimeout(() => this.scrolllLastMessageIntoView());
+      }
+    }
   }
 
+  private scrolllLastMessageIntoView() {
+    const items = this.list.nativeElement.querySelectorAll('li');
+    const lastItem: any = _.last(items);
+    if (lastItem) {
+      lastItem.scrollIntoView();
+    }
+  }
 }
