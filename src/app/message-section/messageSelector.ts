7078d9283582afc84a2c5a84fc13e9f115742c12
Wanasak <u510610433@gmail.com>
2017-08-19 15:40:16 +0700

    using lodash memoized to optimize view redering

--- src/app/message-section/messageSelector.ts
+++ src/app/message-section/messageSelector.ts
@@ -1,4 +1,4 @@
-import {createSelector} from 'reselect/lib';
+import { createSelector } from 'reselect/lib';
 import { Participant } from './../../../shared/model/participant';
 import { Message } from './../../../shared/model/message';
 import { ApplicationState } from './../store/application-state';
@@ -6,26 +6,45 @@ import { MessageVM } from './message.vm';
 
 import * as _ from 'lodash';
 
-export const messagesSelector = createSelector(getParticipants, getMessageForCurrentThread, mapMessagesToMessageVM);
+export const messagesSelector = createSelector(
+  getParticipants,
+  getMessageForCurrentThread,
+  mapMessagesToMessageVM
+);
 
 function getMessageForCurrentThread(state: ApplicationState): Message[] {
-    const currentThread = state.storeData.threads[state.uiState.currentThreadId];
-    return currentThread ? currentThread.messageIds.map(id => state.storeData.messages[id]) : [];
+  const currentThread = state.storeData.threads[state.uiState.currentThreadId];
+  return currentThread
+    ? currentThread.messageIds.map(id => state.storeData.messages[id])
+    : [];
 }
 
 function getParticipants(state: ApplicationState) {
-    return state.storeData.participants;
+  return state.storeData.participants;
 }
 
-function mapMessagesToMessageVM(participants: {[key: number]: Participant}, messages: Message[]) {
-    return messages.map(msg => mapMessageToMessageVM(participants, msg));
+function mapMessagesToMessageVM(
+  participants: { [key: number]: Participant },
+  messages: Message[]
+) {
+  return messages.map(msg => {
+    const participantName = participants[msg.participantId].name;
+    return mapMessageToMessageVM(participantName, msg);
+  });
 }
 
-function mapMessageToMessageVM(participants: {[key: number]: Participant}, message: Message): MessageVM {
-    return {
-        id: message.id,
-        text: message.text,
-        timestamp: message.timestamp,
-        participantName: participants[message.participantId].name
-    };
-}
+// memoized is a process to cache the result which is the same as last calculation
+const mapMessageToMessageVM = _.memoize((
+  participantName: string,
+  message: Message
+) => {
+  return {
+    id: message.id,
+    text: message.text,
+    timestamp: message.timestamp,
+    participantName: participantName
+  };
+},
+    // Define a key for memoized
+    (participantName: string, message: Message) => message.id + participantName
+);
